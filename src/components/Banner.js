deb5a0fde40d78e2e851b1b5ef6ba0a32a9d5931
crazydevhero <bstuqopavyrwku8251@gmail.com>
2022-09-22 17:38:37 -0400

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import styled from "styled-components";
 
-import "../styles/Banner.css";
+import Chart from "../images/chart.png";
+
+const Wrapper = styled.div`
+  background-color: #1d2025;
+  min-height: 50px;
+  min-width: 500px;
+  padding: 20px;
+  align-items: center;
+`
+
+const CaptionWrapper = styled.div`
+  text-align: left;
+  display: flex;
+  flex-direction: row;
+`
+
+const Caption = styled.div`margin-left: 20px; flex: 1`
+const CaptionTitle = styled.div`font-weight: bold; font-size: 30px`
+const CaptionContent = styled.div`font-size: 20px`
+
+const SearchBox = styled.input`
+  background: transparent;
+  border: 0px;
+  color: white;
+  font-size: 15pt;
+  padding: 10px;
+  width: 100%;
+  flex-grow: 1;
+  &:focus  {
+    outline: none;
+  }
+`
 
 export const Banner = () => {
   return (
-    <div>Banner</div>
+    <Wrapper>
+      <CaptionWrapper>
+        <img src={Chart} alt='Price Chart' />
+        <Caption>
+          <CaptionTitle>Crypto<br />Currency</CaptionTitle>
+          <CaptionContent>100 coins</CaptionContent>
+        </Caption>
+      </CaptionWrapper>
+      <SearchBox placeholder='Search by Crypto name (eg: BNB)' />
+    </Wrapper>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,47 @@
 import React from 'react'
-import "../styles/Header.css"
+import styled from "styled-components";
+// import "../styles/Header.css"
 import { IoIosSettings, IoIosMic } from "react-icons/io";
 
+const Wrapper = styled.div`
+    background-color: #1a1015;
+    min-height: 50px;
+    min-width: 500px;
+    padding-left: 20px;
+    padding-right: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const CurrentYear = styled.div`
+    flex: 1;
+    justify-items: flex-start;
+    font-weight: bold;
+    font-size: 15pt;
+    text-align: left;
+`
+
+const Caption = styled.div`
+    flex: 1;
+    justify-items: flex-center;
+    font-size: 20pt;
+`
+
+const Icons = styled.div`
+    flex: 1;
+    justify-items: flex-end;
+    font-weight: bold;
+    font-size: 18pt;
+    text-align: right;
+`
+
 export const Header = () => (
-    <div className='wrapper'>
-        <div className='left'>{new Date().getFullYear()}</div>
-        <div className='center'>Stats</div>
-        <div className='right'>
+    <Wrapper>
+        <CurrentYear>{new Date().getFullYear()}</CurrentYear>
+        <Caption>Stats</Caption>
+        <Icons>
             <IoIosMic />
             <IoIosSettings />
-        </div>
-    </div>
+        </Icons>
+    </Wrapper>
 )
